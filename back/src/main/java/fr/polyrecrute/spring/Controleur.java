2cc9b0d5711f9bc2de41d039a49b28cc450f4aab
MatthieuBalmont <matthieu.balmont@hotmail.fr>
2020-10-22 14:07:55 +0200

    Init openApi to generate documentation

--- back/src/main/java/fr/polyrecrute/spring/Controleur.java
+++ back/src/main/java/fr/polyrecrute/spring/Controleur.java
@@ -1,18 +1,48 @@
 package fr.polyrecrute.spring;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @EnableAutoConfiguration
+@Tag(name = "contact", description = "the Contact API")
 public class Controleur {
 
-    @RequestMapping("/")
+    @Operation(summary = "Find contact by ID", description = "Returns a single contact", tags = { "contact" })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "successful operation",
+                    content = @Content(schema = @Schema(implementation = Controleur.class))),
+            @ApiResponse(responseCode = "404", description = "Contact not found") })
+    @GetMapping(value = "/hello", produces = { "application/json", "application/xml" })
     public String home() {
         return "Hello World!";
     }
+
+    @Operation(summary = "Get a book by its id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the book",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = long.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid id supplied",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Book not found",
+                    content = @Content) })
+    @GetMapping("/hello/{id}")
+    public long findById(@Parameter(description = "id of book to be searched")
+                         @PathVariable long id) {
+        return id;
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(Controleur.class, args);
