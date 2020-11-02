30b4da4d3bb0b6590acdbeeec7bc6398750149fb
argiraud <a.giraud43@outlook.fr>
2020-11-02 14:57:50 +0100

    [7] Page d'accueil V1

---
+++ front/src/components/Navigation.vue
@@ -0,0 +1,81 @@
+<template>
+  <header >
+    <v-app-bar
+        color="primary lighten-1"
+        dark
+        fixed
+    >
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Ptut</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn>Se Connecter</v-btn>
+    </v-app-bar>
+    <v-navigation-drawer
+        v-model="drawer"
+        absolute
+        temporary>
+      <v-list nav dense>
+        <v-list-item-group
+            v-model="group"
+            active-class="lighten-1--text text--accent-4">
+          <v-list-item to="home">
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Accueil</v-list-item-title>
+          </v-list-item>
+          <v-list-item to="home1">
+            <v-list-item-icon>
+              <v-icon>mdi-database-cog</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Administration des données</v-list-item-title>
+          </v-list-item>
+          <v-list-item to="home2">
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Dépôt d'offre d'apprentissage
+              / de candidature</v-list-item-title>
+          </v-list-item>
+          <v-list-item to="home3">
+            <v-list-item-icon>
+              <v-icon>mdi-clipboard-list</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Saisie des voeux</v-list-item-title>
+          </v-list-item>
+          <v-list-item to="home4">
+            <v-list-item-icon>
+              <v-icon>mdi-calendar-month</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Planification d'entretiens</v-list-item-title>
+          </v-list-item>
+          <v-list-item to="home5">
+            <v-list-item-icon>
+              <v-icon>mdi-desktop-mac-dashboard</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Tableau de bord</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </header>
+</template>
+
+<script>
+// import {Slide} from 'vue-burger-menu'
+
+export default {
+  name: 'home',
+  components: {
+    // Slide
+  },
+  data: () => ({
+    drawer: false,
+    group: null,
+  }),
+}
+</script>
+
+<style>
+</style>
--- front/src/main.js
+++ front/src/main.js
@@ -1,4 +1,33 @@
-import { createApp } from 'vue'
 import App from './App.vue'
+import Vue from 'vue'
+import VueRouter from 'vue-router';
+import { routes } from './route';
+import vuetify from './plugins/vuetify';
 
-createApp(App).mount('#app')
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    routes,
+    mode: 'history',
+    scrollBehavior(to, from, savedPosition) {
+        if (savedPosition) {
+            return savedPosition;
+        }
+        if (to.hash) {
+            return { selector: to.hash };
+        }
+        return {x: 0, y: 0};
+    }
+});
+
+router.beforeEach((to, from, next) => {
+    console.log('global beforeEach');
+    next();
+});
+
+new Vue({
+    el: '#app',
+    router,
+    vuetify,
+    render: h => h(App)
+})
