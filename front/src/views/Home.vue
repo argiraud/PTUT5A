a8598ddf395e96dc930b95a6d222ed37da1c798d
argiraud <a.giraud43@outlook.fr>
2020-11-02 14:57:50 +0100

    [7] Page d'accueil V1

---
+++ front/src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-container>
+    <v-img
+        lazy-src="https://upload.wikimedia.org/wikipedia/commons/b/bb/Polytech_Lyon.jpg"
+        max-height="500"
+        src="https://upload.wikimedia.org/wikipedia/commons/b/bb/Polytech_Lyon.jpg"
+    ></v-img>
+    <v-card elevation="2" style="margin-top: 30px">
+      <v-card-title>Polytech</v-card-title>
+      <v-card-text>Hac ita persuasione reducti intra moenia bellatores obseratis undique portarum aditibus, propugnaculis insistebant et pinnis, congesta undique saxa telaque habentes in promptu, ut si quis se proripuisset interius, multitudine missilium sterneretur et lapidum.
+
+        Excitavit hic ardor milites per municipia plurima, quae isdem conterminant, dispositos et castella, sed quisque serpentes latius pro viribus repellere moliens, nunc globis confertos, aliquotiens et dispersos multitudine superabatur ingenti, quae nata et educata inter editos recurvosque ambitus montium eos ut loca plana persultat et mollia, missilibus obvios eminus lacessens et ululatu truci perterrens.
+
+        Tantum autem cuique tribuendum, primum quantum ipse efficere possis, deinde etiam quantum ille quem diligas atque adiuves, sustinere. Non enim neque tu possis, quamvis excellas, omnes tuos ad honores amplissimos perducere, ut Scipio P. Rupilium potuit consulem efficere, fratrem eius L. non potuit. Quod si etiam possis quidvis deferre ad alterum, videndum est tamen, quid ille possit sustinere.</v-card-text>
+    </v-card>
+    <v-card elevation="2" style="margin-top: 30px">
+      <v-card-title>Alternance</v-card-title>
+      <v-card-text>Hac ita persuasione reducti intra moenia bellatores obseratis undique portarum aditibus, propugnaculis insistebant et pinnis, congesta undique saxa telaque habentes in promptu, ut si quis se proripuisset interius, multitudine missilium sterneretur et lapidum.
+
+        Excitavit hic ardor milites per municipia plurima, quae isdem conterminant, dispositos et castella, sed quisque serpentes latius pro viribus repellere moliens, nunc globis confertos, aliquotiens et dispersos multitudine superabatur ingenti, quae nata et educata inter editos recurvosque ambitus montium eos ut loca plana persultat et mollia, missilibus obvios eminus lacessens et ululatu truci perterrens.
+
+        Tantum autem cuique tribuendum, primum quantum ipse efficere possis, deinde etiam quantum ille quem diligas atque adiuves, sustinere. Non enim neque tu possis, quamvis excellas, omnes tuos ad honores amplissimos perducere, ut Scipio P. Rupilium potuit consulem efficere, fratrem eius L. non potuit. Quod si etiam possis quidvis deferre ad alterum, videndum est tamen, quid ille possit sustinere.</v-card-text>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  name: 'home',
+  components: {}
+}
+</script>
+
+<style>
+
+</style>
