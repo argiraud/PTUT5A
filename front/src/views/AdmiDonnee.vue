f7eb2790793f96e7a0370306cb6ac41b26cb88a2
MaximeBaud <maxime.baudin@etu.univ-lyon1.fr>
2020-11-02 16:09:19 +0100

    Form Questionnaire Statisfaction

--- front/src/views/AdmiDonnee.vue
+++ front/src/views/AdmiDonnee.vue
@@ -1,138 +1,71 @@
 <template>
-  <v-form
-      ref="form"
-      v-model="valid"
-      lazy-validation
-  >
-    <v-text-field
-        v-model="name"
-        :rules="nameRules"
-        label="Nom"
-        required
-    ></v-text-field>
+  <div class="about">
+    <p>  Nom : {{ nm }} <input type="text" v-model="nm" > </p>
+    <p>  Prénom : {{ prnm }} <input type="text" v-model="prnm" > </p>
+    <p>  Numéro Etudiant : {{ numEtu }} <input type="text" v-model="numEtu" > </p>
+    <p>  Date de naissance : <input type="date" > </p>
+    <p>  Mail : {{ mail }} <input type="text" v-model="mail" > </p>
+  </div>
 
-    <v-text-field
-        v-model="firstname"
-        :rules="firstnameRules"
-        label="Prénom"
-        required
-    ></v-text-field>
+  <v-simple-table>
+    <template v-slot:default>
+      <thead>
+        <tr>
+            <th class="text-left">Selection</th>
+          <th class="text-left">Nom du document</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="doc in DOC" :key="doc.name">
+                        <td>
+                <label class="form-checkbox">
+                    <input type="checkbox" :value="doc.id" v-model="selected">
+                    <doc class="form-icon"></doc>
+                </label>
+			</td>
+          <td>{{ doc.name }}</td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
 
-    <v-text-field
-        v-model="studentNumber"
-        label="Numéro Etudiant"
-    ></v-text-field>
 
-    <v-menu
-        ref="menu"
-        v-model="menu"
-        :close-on-content-click="false"
-        :nudge-right="40"
-        lazy
-        transition="scale-transition"
-        offset-y
-        full-width
-        min-width="290px"
-    >
-      <template v-slot:activator="{ on }">
-        <v-text-field
-            v-model="date"
-            label="Date de naissance"
-            readonly
-            v-on="on"
-        ></v-text-field>
-      </template>
-      <v-date-picker
-          ref="picker"
-          v-model="date"
-          hint="MM/DD/YYYY format"
-          persistent-hint
-          :max="new Date().toISOString().substr(0, 10)"
-          min="1950-01-01"
-          @change="save"
-      ></v-date-picker>
-    </v-menu>
 
-    <v-text-field
-        v-model="email"
-        :rules="emailRules"
-        label="E-mail"
-        required
-    ></v-text-field>
-
-    <br>
-
-    <v-text-field
-        v-model="presentation"
-        label="Presentation"
-    ></v-text-field>
-
-    <div class="large-12 medium-12 small-12 cell">
-      <label> Document
-        <input type="file" id="file" ref="file" v-on:change="handleFileUpload()"/>
-      </label>
-
-      <button>   Ajouter </button>
-    </div>
-
-    <br>
-
-    <br>
-
-    <v-btn
-        :disabled="!valid"
-        color="Valider"
-        class="mr-4"
-        @click="validate"
-    >
-      Validate
-    </v-btn>
-
-  </v-form>
+<div>
+  <!-- `greet` est le nom de la méthode définie ci-dessous -->
+  <button v-on:click="validate">Valider</button>
+</div>
 
 </template>
 
 
 <script>
-export default {
-  props: ['isClick'],
-  data: () => ({
-    valid: true,
-    name: '',
-    nameRules: [
-      v => !!v || 'Le nom est requis',
-    ],
-    email: '',
-    emailRules: [
-      v => !!v || 'E-mail is required',
-      v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
-    ],
-    firstname: '',
-    firstnameRules: [
-      v => !!v || 'Le prénom est requis',
-    ],
-    studentNumber : '',
-    picker: new Date().toISOString().substr(0, 10),
-    menu: false,
-    date: null,
-    presentation:'',
-    file:''
-  }),
-  watch: {
-    menu (val) {
-      val && setTimeout(() => (this.$refs.picker.activePicker = 'YEAR'))
+  export default {
+    data () {
+      return {
+        DOC: [
+          {
+            name: 'Mon_CV.pdf',
+          },
+          {
+            name: 'Portfolio.pdf',
+          },
+          {
+            name: 'BG.pdf',
+          },       
+        ],
+      }
+    },
+
+    methods: {
+    validate: function (event) {
+      // `this` fait référence à l'instance de Vue à l'intérieur de `methods`
+      this.$router.push('about')
+      // `event` est l'évènement natif du DOM
+      if (event) {
+        //alert(event.target.tagName)
+      }
     }
-  },
-  methods: {
-    validate () {
-      this.$emit("message-sent", "true")
-      this.$refs.form.validate();
-
-    },
-
-    handleFileUpload(){
-      this.file = this.$refs.file.files[0];
-    },
-  },
-}
+  }
+  }
 </script>
