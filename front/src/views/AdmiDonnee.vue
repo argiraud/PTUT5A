67a883c2960ba49f18806c9765f4ec8e4e00ea5c
argiraud <a.giraud43@outlook.fr>
2020-11-02 16:24:22 +0100

    Correction erreur suite rebase branche dev

--- front/src/views/AdmiDonnee.vue
+++ front/src/views/AdmiDonnee.vue
@@ -1,63 +1,64 @@
 <template>
-  <div class="about">
-    <p>  Nom : {{ nm }} <input type="text" v-model="nm" > </p>
-    <p>  Prénom : {{ prnm }} <input type="text" v-model="prnm" > </p>
-    <p>  Numéro Etudiant : {{ numEtu }} <input type="text" v-model="numEtu" > </p>
-    <p>  Date de naissance : <input type="date" > </p>
-    <p>  Mail : {{ mail }} <input type="text" v-model="mail" > </p>
-  </div>
+  <div>
+    <div class="about">
+      <p> Nom : {{ nm }} <input type="text" v-model="nm"></p>
+      <p> Prénom : {{ prnm }} <input type="text" v-model="prnm"></p>
+      <p> Numéro Etudiant : {{ numEtu }} <input type="text" v-model="numEtu"></p>
+      <p> Date de naissance : <input type="date"></p>
+      <p> Mail : {{ mail }} <input type="text" v-model="mail"></p>
+    </div>
 
-  <v-simple-table>
-    <template v-slot:default>
-      <thead>
+    <v-simple-table>
+      <template v-slot:default>
+        <thead>
         <tr>
-            <th class="text-left">Selection</th>
+          <th class="text-left">Selection</th>
           <th class="text-left">Nom du document</th>
         </tr>
-      </thead>
-      <tbody>
+        </thead>
+        <tbody>
         <tr v-for="doc in DOC" :key="doc.name">
-                        <td>
-                <label class="form-checkbox">
-                    <input type="checkbox" :value="doc.id" v-model="selected">
-                    <doc class="form-icon"></doc>
-                </label>
-			</td>
+          <td>
+            <label class="form-checkbox">
+              <input type="checkbox" :value="doc.id" v-model="selected">
+              <doc class="form-icon"></doc>
+            </label>
+          </td>
           <td>{{ doc.name }}</td>
         </tr>
-      </tbody>
-    </template>
-  </v-simple-table>
+        </tbody>
+      </template>
+    </v-simple-table>
 
 
-
-<div>
-  <!-- `greet` est le nom de la méthode définie ci-dessous -->
-  <button v-on:click="validate">Valider</button>
-</div>
+    <div>
+      <!-- `greet` est le nom de la méthode définie ci-dessous -->
+      <button v-on:click="validate">Valider</button>
+    </div>
+  </div>
 
 </template>
 
 
 <script>
-  export default {
-    data () {
-      return {
-        DOC: [
-          {
-            name: 'Mon_CV.pdf',
-          },
-          {
-            name: 'Portfolio.pdf',
-          },
-          {
-            name: 'BG.pdf',
-          },       
-        ],
-      }
-    },
+export default {
+  data() {
+    return {
+      DOC: [
+        {
+          name: 'Mon_CV.pdf',
+        },
+        {
+          name: 'Portfolio.pdf',
+        },
+        {
+          name: 'BG.pdf',
+        },
+      ],
+    }
+  },
 
-    methods: {
+  methods: {
     validate: function (event) {
       // `this` fait référence à l'instance de Vue à l'intérieur de `methods`
       this.$router.push('about')
@@ -67,5 +68,5 @@
       }
     }
   }
-  }
+}
 </script>
