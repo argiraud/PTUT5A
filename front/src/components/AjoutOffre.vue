6ef869affb401aa6db3b2474cb6637a3734f2506
MaximeBaud <maxime.baudin@etu.univ-lyon1.fr>
2020-11-19 15:04:17 +0100

    [12] WIP Ajout Offre et Depot Candidature 2

--- front/src/components/AjoutOffre.vue
+++ front/src/components/AjoutOffre.vue
@@ -37,7 +37,17 @@
       </v-card-actions>
 
       <v-card-actions>
+      <div class="large-12 medium-12 small-12 cell">
+        <label> Document
+          <input type="file" id="file" ref="file" @change="onFileChange"/>
+        </label>
+
+      </div>
+      </v-card-actions>
+
+      <v-card-actions>
         <v-btn color="primary" @click="AddOffer">Ajouter</v-btn>
+        <v-btn color="primary" @click="this.APIAddDocument">Ajouter File</v-btn>
         <v-btn color="red" flat @click.stop="show=false">Fermer</v-btn>
       </v-card-actions>
     </v-card>
@@ -48,6 +58,7 @@
 export default {
 name: "AjoutOffre",
   data: () => ({
+    filesObj:{},
     valid: true,
     title: '',
     titleRules: [
@@ -72,6 +83,51 @@ name: "AjoutOffre",
   methods: {
     AddOffer(){
         this.APIAddOffer();
+        //this.APIAddDocument();
+    },
+    APIAddDocument(){
+      const offer = {
+        file: this.filesObj,
+        idOffer: window.sessionStorage.getItem("idOffer"),
+      };
+      alert("test");
+      alert(window.sessionStorage.getItem("idOffer"));
+      const options = {
+        method: 'POST',
+        body: JSON.stringify(offer),
+        headers: {
+          "accept": "*/*",
+          "Authorization": window.sessionStorage.getItem("UserToken"),
+          "Content-Type": "application/json"
+        }
+      };
+      fetch("https://api.polyrecrute.tk/company/offer/uploadFile", options).then(response => {
+        console.log(response);
+        switch (response.status) {
+          case 201 :
+            alert("Ajout du document effectuée");
+            break;
+          case 400 :
+            alert("Title, key word or description is too long");
+            break;
+          case 401 :
+            alert("Authentification error");
+            break;
+          case 403:
+            alert("No sufficient right");
+            break;
+        }
+        return response.json();
+      }).then(data => {
+        console.log(data);
+      }).catch(err => {
+        console.log(err);
+      });
+    },
+    onFileChange(event){
+      let fileData =  event.target.files[0];
+      this.filesObj = fileData;
+      console.log('file Object:==>',this.filesObj);
     },
     APIAddOffer(){
 
