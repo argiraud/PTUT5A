c8372fe9a58837dd776578e18aa3b7a62aa04feb
MaximeBaud <45593166+MaximeBaud@users.noreply.github.com>
2021-01-18 16:21:32 +0100

    [12] WIP Depot document Entreprise [14] Saisie des voeux Entreprise (#50)
    * Form Questionnaire Statisfaction
    
    * Correction erreur suite rebase branche dev
    
    * [28] Stepper avancement (création Depot offre/ Saisie des voeux)
    
    * [28] Stepper terminé
    
    * [28] Stepper terminé 2
    
    * WIP Depot Offre
    
    * [12] WIP Ajout Offre et Depot Candidature
    
    * [12] WIP Ajout Offre et Depot Candidature 2
    
    * [12] WIP Ajout Offre et Depot Candidature [14] WIP Saisie des voeux
    
    Co-authored-by: argiraud <a.giraud43@outlook.fr>
    Co-authored-by: argiraud <33487226+argiraud@users.noreply.github.com>

---
+++ front/src/components/AjoutOffre.vue
@@ -0,0 +1,161 @@
+<template>
+  <v-dialog v-model="show" max-width="500px">
+    <v-card>
+      <v-card-actions>
+        <v-text-field
+            v-model="title"
+            :rules="titleRules"
+            id="title"
+            label="Titre de l'offre"
+            required
+            type="text"
+            color="teal accent-3"
+            prepend-inner-icon="person"
+        ></v-text-field>
+      </v-card-actions>
+
+      <v-card-actions>
+        <v-text-field
+            v-model="keyWord"
+            id="keyWord"
+            label="Mots clés"
+            type="text"
+            color="teal accent-3"
+            prepend-inner-icon="person"
+        ></v-text-field>
+      </v-card-actions>
+
+      <v-card-actions>
+        <v-text-field
+            v-model="description"
+            id="description"
+            label="Description"
+            type="text"
+            color="teal accent-3"
+            prepend-inner-icon="person"
+        ></v-text-field>
+      </v-card-actions>
+
+      <v-card-actions>
+      <div class="large-12 medium-12 small-12 cell">
+        <label> Document
+          <input type="file" id="file" ref="file" @change="onFileChange"/>
+        </label>
+
+      </div>
+      </v-card-actions>
+
+      <v-card-actions>
+        <v-btn color="primary" @click="AddOffer">Ajouter</v-btn>
+        <v-btn color="primary" @click="this.APIAddDocument">Ajouter File</v-btn>
+        <v-btn color="red" flat @click.stop="show=false">Fermer</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import CompanyDataService from "@/service/CompanyDataService";
+
+export default {
+name: "AjoutOffre",
+  data: () => ({
+    filesObj:{},
+    valid: true,
+    title: '',
+    titleRules: [
+      v => !!v || 'le titre est requis'
+    ],
+    keyWord: '',
+    description: '',
+  }),
+  props: {
+    value: Boolean
+  },
+  computed: {
+    show: {
+      get () {
+        return this.value
+      },
+      set (value) {
+        this.$emit('input', value)
+      }
+    }
+  },
+  methods: {
+    AddOffer(){
+        this.APIAddOffer();
+        //this.APIAddDocument();
+    },
+    APIAddDocument(){
+      const offer = {
+        file: this.filesObj,
+        idOffer: window.sessionStorage.getItem("idOffer"),
+      };
+      alert("test");
+      alert(window.sessionStorage.getItem("idOffer"));
+      const options = {
+        method: 'POST',
+        body: JSON.stringify(offer),
+        headers: {
+          "accept": "*/*",
+          "Authorization": window.sessionStorage.getItem("UserToken"),
+          "Content-Type": "application/json"
+        }
+      };
+      fetch("https://api.polyrecrute.tk/company/offer/uploadFile", options).then(response => {
+        console.log(response);
+        switch (response.status) {
+          case 201 :
+            alert("Ajout du document effectuée");
+            break;
+          case 400 :
+            alert("Title, key word or description is too long");
+            break;
+          case 401 :
+            alert("Authentification error");
+            break;
+          case 403:
+            alert("No sufficient right");
+            break;
+        }
+        return response.json();
+      }).then(data => {
+        console.log(data);
+      }).catch(err => {
+        console.log(err);
+      });
+    },
+    onFileChange(event){
+      let fileData =  event.target.files[0];
+      this.filesObj = fileData;
+      console.log('file Object:==>',this.filesObj);
+    },
+    APIAddOffer(){
+      const offer = {
+        title: document.getElementById('title').value.toString(),
+        keyWord: document.getElementById('keyWord').value.toString(),
+        description: document.getElementById('description').value.toString(),
+      };
+      CompanyDataService.create(offer).then(response => {
+        switch (response.status) {
+          case 201 :
+            window.sessionStorage.setItem("idOffer", response.data.idOffer);
+            window.sessionStorage.setItem("idEntity", response.data.company.idEntity);
+            alert("Ajout du document effectuée");
+            break;
+        }
+      })
+          .catch(e => {
+            console.error(e);
+          })
+
+    }
+  },
+}
+
+</script>
+
+<style scoped>
+
+</style>
