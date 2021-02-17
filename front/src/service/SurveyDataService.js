abbec4395df354a37f9b3beb04c1e8b8cefdfc8b
argiraud <a.giraud43@outlook.fr>
2021-02-17 17:24:22 +0100

    Survey : validation button disabled when form invalid + SurveyDataService creation

---
+++ front/src/service/SurveyDataService.js
@@ -0,0 +1,48 @@
+import {axiosAPI} from "@/http-common";
+
+class SurveyDataService {
+    getAll() {
+        return axiosAPI.get("/students");
+    }
+
+    count(){
+        return axiosAPI.get("/kpi/users");
+    }
+
+     getUserDetails(userId) {
+         return axiosAPI.get(`/user/details/${userId}`);
+     }
+
+     getConnectedUser() {
+         return axiosAPI.get(`/user/details`);
+     }
+
+    setNewUserPassword(password) {
+        return axiosAPI.patch(`/user/password?password=${password}`);
+    }
+
+    updateUserInfos(user){ //On envoie un utilisateur au format JSON
+        return axiosAPI.patch(`/user`,user);
+    }
+    // create(data) {
+    //     return http.post("/tutorials", data);
+    // }
+    //
+    // update(id, data) {
+    //     return http.put(`/tutorials/${id}`, data);
+    // }
+    //
+    // delete(id) {
+    //     return http.delete(`/tutorials/${id}`);
+    // }
+    //
+    // deleteAll() {
+    //     return http.delete(`/tutorials`);
+    // }
+    //
+    // findByTitle(title) {
+    //     return http.get(`/tutorials?title=${title}`);
+    // }
+}
+
+export default new SurveyDataService();
--- front/src/views/Survey.vue
+++ front/src/views/Survey.vue
@@ -1,196 +1,206 @@
 <template>
-  <v-container >
+  <v-container>
     <h1 class="text-center" style="margin-bottom: 10px">Questionnaire</h1>
-    <p>{{ 'La communication sur l\'événement a été :' }}</p>
-    <v-radio-group
-        v-model="question1"
-        mandatory
-        class="radio-group"
+    <v-form ref="form"
+            v-model="valid"
     >
-      <v-radio
-          label="Excellente"
-          value="excellente"
-      ></v-radio>
-      <v-radio
-          label="Bonne"
-          value="bonne"
-      ></v-radio>
-      <v-radio
-          label="Moyenne"
-          value="moyenne"
-      ></v-radio>
-      <v-radio
-          label="Insuffisante"
-          value="insuffisante"
-      ></v-radio>
-    </v-radio-group>
-    <p>{{ 'Les informations délivrées sur l\'événement ont été :' }}</p>
-    <v-radio-group
-        v-model="question2"
-        mandatory
-        class="radio-group"
-    >
-      <v-radio
-          label="Excellentes"
-          value="excellente"
-      ></v-radio>
-      <v-radio
-          label="Bonnes"
-          value="bonne"
-      ></v-radio>
-      <v-radio
-          label="Moyennes"
-          value="moyenne"
-      ></v-radio>
-      <v-radio
-          label="Insuffisantes"
-          value="insuffisante"
-      ></v-radio>
-    </v-radio-group>
-    <p>{{ 'Globalement, est-ce que le Forum virtuel a correspondu à vos attentes ?' }}</p>
-    <v-radio-group
-        v-model="question3"
-        mandatory
-        class="radio-group"
-    >
-      <v-radio
-          label="Tout à fait"
-          value="excellente"
-      ></v-radio>
-      <v-radio
-          label="Plutôt oui"
-          value="bonne"
-      ></v-radio>
-      <v-radio
-          label="Plutôt non"
-          value="moyenne"
-      ></v-radio>
-      <v-radio
-          label="Pas du tout"
-          value="insuffisante"
-      ></v-radio>
-    </v-radio-group>
-    <p>{{
-        'Avez-vous trouvé des profils de candidats (entreprise) et des offres d\'entreprise intéressantes (candidat)?'
-      }}</p>
-    <v-radio-group
-        v-model="question4"
-        mandatory
-        class="radio-group"
-    >
-      <v-radio
-          label="Tout à fait"
-          value="excellente"
-      ></v-radio>
-      <v-radio
-          label="Plutôt oui"
-          value="bonne"
-      ></v-radio>
-      <v-radio
-          label="Plutôt non"
-          value="moyenne"
-      ></v-radio>
-      <v-radio
-          label="Pas du tout"
-          value="insuffisante"
-      ></v-radio>
-    </v-radio-group>
-    <p>{{ 'Avez-vous trouvé l\'application simple d\'utilisation ?' }}</p>
-    <v-radio-group
-        v-model="question5"
-        mandatory
-        class="radio-group"
-    >
-      <v-radio
-          label="Tout à fait"
-          value="excellente"
-      ></v-radio>
-      <v-radio
-          label="Plutôt oui"
-          value="bonne"
-      ></v-radio>
-      <v-radio
-          label="Plutôt non"
-          value="moyenne"
-      ></v-radio>
-      <v-radio
-          label="Pas du tout"
-          value="insuffisante"
-      ></v-radio>
-    </v-radio-group>
-    <p>{{ 'Avez-vous trouvé l\'application simple d\'utilisation ?' }}</p>
-    <v-textarea
-        :rules="questionRule"
-        required
-        name="input-7-1"
-        filled
-        label="Votre réponse"
-        auto-grow
-    ></v-textarea>
-    <p>{{ 'Etes-vous satisfait du déroulement de vos entretiens ?' }}</p>
-    <v-radio-group
-        v-model="question7"
-        mandatory
-        class="radio-group"
-    >
-      <v-radio
-          label="Tout à fait"
-          value="excellente"
-      ></v-radio>
-      <v-radio
-          label="Plutôt oui"
-          value="bonne"
-      ></v-radio>
-      <v-radio
-          label="Plutôt non"
-          value="moyenne"
-      ></v-radio>
-      <v-radio
-          label="Pas du tout"
-          value="insuffisante"
-      ></v-radio>
-    </v-radio-group>
-    <p>{{ 'Avez-vous des remarques à partager sur le déroulement de vos entretiens ?' }}</p>
-    <v-textarea
-        :rules="questionRule"
-        required
-        name="input-7-1"
-        filled
-        label="Votre réponse"
-        auto-grow
-    ></v-textarea>
-    <p>{{ 'Selon vous, quels sont les points positifs du Forum virtuel ?' }}</p>
-    <v-textarea
-        :rules="questionRule"
-        required
-        name="input-7-1"
-        filled
-        label="Votre réponse"
-        auto-grow
-    ></v-textarea>
-    <p>{{ 'Selon vous, quelles sont les améliorations à apporter ?' }}</p>
-    <v-text-field
-        :rules="questionRule"
-        required
-        name="input-7-1"
-        filled
-        label="Votre réponse"
-        auto-grow
-        style="margin-right: 50px"
-    ></v-text-field>
-    <p>{{ 'Avez-vous d\'autres retours/suggestions à partager concernant le Forum virtuel ?' }}</p>
-    <v-textarea
-        :rules="questionRule"
-        required
-        name="input-7-1"
-        filled
-        label="Votre réponse"
-        auto-grow
-    ></v-textarea>
+      <p>{{ 'La communication sur l\'événement a été :' }}</p>
+      <v-radio-group
+          v-model="question1"
+          mandatory
+          class="radio-group"
+      >
+        <v-radio
+            label="Excellente"
+            value="excellente"
+        ></v-radio>
+        <v-radio
+            label="Bonne"
+            value="bonne"
+        ></v-radio>
+        <v-radio
+            label="Moyenne"
+            value="moyenne"
+        ></v-radio>
+        <v-radio
+            label="Insuffisante"
+            value="insuffisante"
+        ></v-radio>
+      </v-radio-group>
+      <p>{{ 'Les informations délivrées sur l\'événement ont été :' }}</p>
+      <v-radio-group
+          v-model="question2"
+          mandatory
+          class="radio-group"
+      >
+        <v-radio
+            label="Excellentes"
+            value="excellente"
+        ></v-radio>
+        <v-radio
+            label="Bonnes"
+            value="bonne"
+        ></v-radio>
+        <v-radio
+            label="Moyennes"
+            value="moyenne"
+        ></v-radio>
+        <v-radio
+            label="Insuffisantes"
+            value="insuffisante"
+        ></v-radio>
+      </v-radio-group>
+      <p>{{ 'Globalement, est-ce que le Forum virtuel a correspondu à vos attentes ?' }}</p>
+      <v-radio-group
+          v-model="question3"
+          mandatory
+          class="radio-group"
+      >
+        <v-radio
+            label="Tout à fait"
+            value="excellente"
+        ></v-radio>
+        <v-radio
+            label="Plutôt oui"
+            value="bonne"
+        ></v-radio>
+        <v-radio
+            label="Plutôt non"
+            value="moyenne"
+        ></v-radio>
+        <v-radio
+            label="Pas du tout"
+            value="insuffisante"
+        ></v-radio>
+      </v-radio-group>
+      <p>{{
+          'Avez-vous trouvé des profils de candidats (entreprise) et des offres d\'entreprise intéressantes (candidat)?'
+        }}</p>
+      <v-radio-group
+          v-model="question4"
+          mandatory
+          class="radio-group"
+      >
+        <v-radio
+            label="Tout à fait"
+            value="excellente"
+        ></v-radio>
+        <v-radio
+            label="Plutôt oui"
+            value="bonne"
+        ></v-radio>
+        <v-radio
+            label="Plutôt non"
+            value="moyenne"
+        ></v-radio>
+        <v-radio
+            label="Pas du tout"
+            value="insuffisante"
+        ></v-radio>
+      </v-radio-group>
+      <p>{{ 'Avez-vous trouvé l\'application simple d\'utilisation ?' }}</p>
+      <v-radio-group
+          v-model="question5"
+          mandatory
+          class="radio-group"
+      >
+        <v-radio
+            label="Tout à fait"
+            value="excellente"
+        ></v-radio>
+        <v-radio
+            label="Plutôt oui"
+            value="bonne"
+        ></v-radio>
+        <v-radio
+            label="Plutôt non"
+            value="moyenne"
+        ></v-radio>
+        <v-radio
+            label="Pas du tout"
+            value="insuffisante"
+        ></v-radio>
+      </v-radio-group>
+      <p>{{ 'Avez-vous trouvé l\'application simple d\'utilisation ?' }}</p>
+      <v-textarea
+          v-model="question6"
+          :rules="questionRule"
+          required
+          name="input-7-1"
+          filled
+          label="Votre réponse"
+          auto-grow
+      ></v-textarea>
+      <p>{{ 'Etes-vous satisfait du déroulement de vos entretiens ?' }}</p>
+      <v-radio-group
+          v-model="question7"
+          mandatory
+          class="radio-group"
+      >
+        <v-radio
+            label="Tout à fait"
+            value="excellente"
+        ></v-radio>
+        <v-radio
+            label="Plutôt oui"
+            value="bonne"
+        ></v-radio>
+        <v-radio
+            label="Plutôt non"
+            value="moyenne"
+        ></v-radio>
+        <v-radio
+            label="Pas du tout"
+            value="insuffisante"
+        ></v-radio>
+      </v-radio-group>
+      <p>{{ 'Avez-vous des remarques à partager sur le déroulement de vos entretiens ?' }}</p>
+      <v-textarea
+          v-model="question8"
+          :rules="questionRule"
+          required
+          name="input-7-1"
+          filled
+          label="Votre réponse"
+          auto-grow
+      ></v-textarea>
+      <p>{{ 'Selon vous, quels sont les points positifs du Forum virtuel ?' }}</p>
+      <v-textarea
+          v-model="question9"
+          :rules="questionRule"
+          required
+          name="input-7-1"
+          filled
+          label="Votre réponse"
+          auto-grow
+      ></v-textarea>
+      <p>{{ 'Selon vous, quelles sont les améliorations à apporter ?' }}</p>
+      <v-textarea
+          v-model="question10"
+          :rules="questionRule"
+          required
+          name="input-7-1"
+          filled
+          label="Votre réponse"
+          auto-grow
+          style="margin-right: 50px"
+      ></v-textarea>
+      <p>{{ 'Avez-vous d\'autres retours/suggestions à partager concernant le Forum virtuel ?' }}</p>
+      <v-textarea
+          v-model="question11"
+          :rules="questionRule"
+          required
+          name="input-7-1"
+          filled
+          label="Votre réponse"
+          auto-grow
+      ></v-textarea>
+    </v-form>
     <div class="text-center">
       <v-btn
           color="Valider"
           class="mr-4"
+          :disabled="!valid"
           @click="validate"
       >
         Valider
@@ -203,6 +213,18 @@
 export default {
   name: "Survey",
   data: () => ({
+    valid: false,
+    question1: '',
+    question2: '',
+    question3: '',
+    question4: '',
+    question5: '',
+    question6: '',
+    question7: '',
+    question8: '',
+    question9: '',
+    question10: '',
+    question11: '',
     questionRule: [
       v => !!v || 'Une réponse est requise'
     ],
@@ -217,11 +239,6 @@ export default {
 </script>
 
 <style scoped>
-.questions {
-  margin-left: 50px;
-  margin-right: 50px;
-}
-
 .radio-group {
   margin-left: 70px;
 }
