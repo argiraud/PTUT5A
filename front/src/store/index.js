c9733cec5a30df71ed5145b2b25d07f2119140e4
Lucas Ferragut <lucas.ferragut42@gmail.com>
2021-02-18 17:42:12 +0100

    Visionnage d'un autre profil que le sien OK

--- front/src/store/index.js
+++ front/src/store/index.js
@@ -17,6 +17,18 @@ export default new Vuex.Store({
       Presentation: '',
       RoleId: '',
       IsAdmin: ''
+    },
+    profileUserInfos : {
+      Id: '',
+      Name : '',
+      Firstname : '',
+      StudentNumber : '',
+      BirthDate: '',
+      Email: '',
+      Status: '',
+      Presentation: '',
+      RoleId: '',
+      IsAdmin: ''
     }
   },
   mutations: {
@@ -25,7 +37,7 @@ export default new Vuex.Store({
     },
     // eslint-disable-next-line no-unused-vars
     SET_CURRENTUSER_FROM_JSON(state, payload){
-      console.log("payload :" + payload);
+      console.log("SET_CURRENTUSER_FROM_JSON");
       state.currentUser.Id = payload.id != undefined ? payload.id : '';
       state.currentUser.Name = payload.name != undefined ? payload.name : '';
       state.currentUser.Firstname = payload.firstName != undefined ? payload.firstName : '';
@@ -34,16 +46,32 @@ export default new Vuex.Store({
       state.currentUser.Email = payload.email != undefined ? payload.email : '';
       state.currentUser.Status = payload.status != undefined ? payload.status : '';
       state.currentUser.Presentation = payload.presentation != undefined ? payload.presentation : '';
-      state.currentUser.RoleId = payload.roles[0].idRole != undefined ? payload.roles[0].idRole : '';
-      state.currentUser.IsAdmin = payload.roles[1] != undefined ? true : false;
+      if(typeof(payload.roles) != undefined ){
+        state.currentUser.RoleId = payload.roles[0] != undefined ? payload.roles[0].idRole : '';
+        state.currentUser.IsAdmin = payload.roles[1] != undefined ? true : false;
+      }else{
+        state.currentUser.RoleId = '';
+        state.currentUser.IsAdmin = false;
+      }
 
-      /*
-      window.sessionStorage.setItem("UserName", payload.name);
-      window.sessionStorage.setItem("UserEmail", payload.email);
-      window.sessionStorage.setItem("UserId", payload.id);
-      window.sessionStorage.setItem("UserPresentation", payload.presentation);
-      window.sessionStorage.setItem("UserRoleId", payload.roles[0].idRole);
-      window.sessionStorage.setItem("UserIsAdmin", payload.roles[1] != undefined ? true : false);*/
+    },
+    SET_PROFILEUSERINFOS_FROM_JSON(state, payload){
+      console.log("SET_PROFILEUSERINFOS_FROM_JSON");
+      state.profileUserInfos.Id = payload.id != undefined ? payload.id : '';
+      state.profileUserInfos.Name = payload.name != undefined ? payload.name : '';
+      state.profileUserInfos.Firstname = payload.firstName != undefined ? payload.firstName : '';
+      state.profileUserInfos.StudentNumber = payload.etudiantNumber != undefined ? payload.etudiantNumber : '';
+      state.profileUserInfos.BirthDate = payload.birthDate != undefined ? payload.birthDate : '';
+      state.profileUserInfos.Email = payload.email != undefined ? payload.email : '';
+      state.profileUserInfos.Status = payload.status != undefined ? payload.status : '';
+      state.profileUserInfos.Presentation = payload.presentation != undefined ? payload.presentation : '';
+      if(typeof(payload.roles) != "undefined" ){
+        state.profileUserInfos.RoleId = payload.roles[0] != undefined ? payload.roles[0].idRole : '';
+        state.profileUserInfos.IsAdmin = payload.roles[1] != undefined ? true : false;
+      }else {
+        state.profileUserInfos.RoleId = '';
+        state.profileUserInfos.IsAdmin = false;
+      }
     },
     SET_CURRENTUSERNAME(state, payload){
       state.currentUser.Name = payload;
